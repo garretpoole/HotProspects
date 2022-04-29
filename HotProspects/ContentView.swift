3a1c268b8a057176f86391815161b1b7cc94ef97
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-29 13:22:19 -0700

    controlling image interpolation of small pixelated images

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,35 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        //result will hold the entire success or entire error inside of itself which can then be stored and passed until ready to use
-        let result = await fetchTask.result
-//        do {
-//            output = try result.get()
-//        } catch {
-//            print("Download error")
-//        }
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download Error: \(error.localizedDescription)"
-        }
+        //very small image so blended pixels standout
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
 }
 
