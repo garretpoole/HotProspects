ad59cbe789b37c894742fdf70c66f38a94d57b45
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-29 12:44:49 -0700

    @EnvironmentObject example

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    //automatically looks for User instance in Environment and uses what it finds
+    //if no user instance in Environment, program will crash
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    @StateObject var user = User()
+    
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        //places an object into Environment so any child view can have access to it
+        .environmentObject(user)
     }
 }
 
