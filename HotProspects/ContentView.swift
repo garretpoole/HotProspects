44ec1c33f8ece24125d5ffa4d890c4da783f08e3
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-07 11:29:56 -0700

    context menus

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
-        //very small image so blended pixels standout
-        Image("example")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
+        VStack {
+            Text("hello")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+            //if used, used them consistently
+            //keep list short (3 max)
+            //dont repeat options user can see elsewhere in UI
+            //hidden so dont make it important
+                .contextMenu {
+                    Button(role: .destructive) {
+                        backgroundColor = .red
+                    } label: {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.red)
+                    }
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                }
+        }
     }
 }
 
