86fae99a3e23c93a745bc45e6dcc82f9ae3f7a9a
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-07 16:16:50 -0700

    start of HotProspects

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,20 +5,11 @@
 //  Created by Garret Poole on 4/29/22.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibeNumbers = Array(1...60)
-    
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibeNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
+        Text("Hello world")
     }
 }
 
